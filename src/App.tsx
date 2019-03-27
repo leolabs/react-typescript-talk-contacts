3e0908e1e6ab5aa759210bd3a8969985355cf061
Leo Bernard <admin@leolabs.org>
2019-03-27 18:50:28 +0100

    :sparkles: Use React FC for main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import { contacts, Person } from "./data";
 import { ContactCard } from "./contact-card";
 
-class App extends Component {
-  sortByLastname(a: Person, b: Person) {
-    return [...a.names].pop()!.localeCompare([...b.names].pop()!);
-  }
+const sortByLastname = (a: Person, b: Person) => {
+  return [...a.names].pop()!.localeCompare([...b.names].pop()!);
+};
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>My Contacts</h1>
-        </header>
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>My Contacts</h1>
+    </header>
 
-        <main>
-          {contacts.sort(this.sortByLastname).map(c => (
-            <ContactCard key={c.email} person={c} />
-          ))}
-        </main>
-      </div>
-    );
-  }
-}
+    <main>
+      {contacts.sort(sortByLastname).map(c => (
+        <ContactCard key={c.email} person={c} />
+      ))}
+    </main>
+  </div>
+);
 
 export default App;
